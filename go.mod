a9d741b47426a48c915e6c26952c7d4f66501127
SleeplessSheep <sleepysheep326@proton.me>
2025-12-07 20:50:09 +0800

    FEAT: Add login route and Google redirect

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module go-oauth
 
 go 1.25.4
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require (
+	github.com/joho/godotenv v1.5.1
+	golang.org/x/oauth2 v0.33.0
+)
+
+require cloud.google.com/go/compute/metadata v0.3.0 // indirect
--- main.go
+++ main.go
@@ -7,7 +7,11 @@ import (
 	"os"
 
 	"github.com/joho/godotenv"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
 )
+
+var googleOauthConfig *oauth2.Config
 
 func main() {
 	// load the .env file
@@ -16,19 +20,29 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
-	// testing the key
-	clientID := os.Getenv("GOOGLE_CLIENT_ID")
-	clientSecret := os.Getenv("GOOGLE_CLIENT_SECRET")
-
-	if clientID == "" || clientSecret == "" {
-		log.Fatal("Environment variables not set")
+	googleOauthConfig = &oauth2.Config{
+		ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
+		ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
+		RedirectURL:  "http://localhost:8080/callback",
+		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email"},
+		Endpoint:     google.Endpoint,
 	}
 
 	// Basic endpoint
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Server is running with secure keys!")
-	})
+	http.HandleFunc("/", handleHome)
+	http.HandleFunc("/login", handleLogin)
 
 	fmt.Println("Server running on http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
+}
+
+func handleHome(w http.ResponseWriter, r *http.Request) {
+	var html = `<html><body><a href="/login">Google Login</a></body></html>`
+	fmt.Fprint(w, html)
+}
+
+func handleLogin(w http.ResponseWriter, r *http.Request) {
+	// state should be update as real random func, but set it as fixed string for testing
+	url := googleOauthConfig.AuthCodeURL("random")
+	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
 }
